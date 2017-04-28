81de623bb3cf8a00b7e4f191a62f64c1ac352dcf
Yi Qiao Wang <yiqiao.wang96@gmail.com>
2017-04-27 21:09:56 -0400

    Add use-agent line

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stow -v ansiweather bin bspwm emacs mpd ncmpcpp polybar rofi sxhkd systemd X zsh
+stow -v ansiweather bin bspwm emacs mpd ncmpcpp polybar rofi sxhkd systemd X zsh gnupg
